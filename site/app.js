658ce5864c6daa3d3ce67f7b12a71efce06f969e
Adam Hammes <adam@hammes.io>
2018-01-30 11:11:02 -0600

    First map render (WIP)

--- site/app.js
+++ site/app.js
@@ -1,14 +1,30 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import * as L from "leaflet";
+import * as apartment_json from "../example_values.json";
 
-const Test = (message) => {
-	return (
-		<div>{message}</div>
-	);
+const apartments = Array.from(Object.values(apartment_json));
+console.log(apartments);
+
+const quebecLocation = {
+	lat: 46.82,
+	lng: -71.3,
 };
 
-ReactDOM.render(
-	<h1>Hello, world!</h1>,
-	document.getElementById('root')
-);
+const minZoom = 11;
 
+const map = L.map('mapId').setView(quebecLocation, minZoom);
+
+const accessToken = 'pk.eyJ1IjoiYWRhbWhhbW1lcyIsImEiOiJjamQxczNrajQyd25kMndvNWR6cGdqYWl2In0.30k-mIhdJr0otiiSv8mQ-w';
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Todo',
+	minZoom: minZoom,
+	id: 'mapbox.streets',
+	accessToken: accessToken
+}).addTo(map);
+
+apartments.forEach(apartment => {
+	const marker = L.marker([apartment.latitude, apartment.longitude]).addTo(map);
+	marker.bindPopup(apartment.description);
+})
+// for (let i = 0; i < 2000; i++) {
+// }
