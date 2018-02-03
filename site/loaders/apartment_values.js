7a6ddc9d8be4ae20058babb9688c514353474692
Adam Hammes <adam@hammes.io>
2018-02-03 16:14:14 -0600

    Use kijiji output directory as default data source
    Closes #21

--- site/loaders/apartment_values.js
+++ site/loaders/apartment_values.js
@@ -4,7 +4,19 @@ const fs = require("fs");
 // To change which data is loaded, pass the --env.values_path option to webpack.
 module.exports = content => {
   const defaultLocation = path.resolve(process.cwd(), "example_values.json");
-  const jsonPath = process.env.values_path || defaultLocation;
+  let jsonPath = defaultLocation;
+
+  if (process.env.values_path) {
+    jsonPath = process.env.values_path;
+  } else if (process.env.KIJIJI_OUTPUT_DIRECTORY) {
+    const potentialLocation = path.resolve(
+      process.env.KIJIJI_OUTPUT_DIRECTORY,
+      "trimmed_values.json"
+    );
+    if (fs.existsSync(potentialLocation)) {
+      jsonPath = potentialLocation;
+    }
+  }
 
   const json = fs.readFileSync(jsonPath);
   return `module.exports = ${json}`;
