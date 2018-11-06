be1badf00fdc699f64c65686a09cc558be51ea95
Adam Hammes <ahammes@cortexmedia.ca>
2018-11-06 09:34:38 -0500

    Update frontend to use new data
    Filter are temporarily disabled while I try to figure out which housing
    types take which filters.

--- site/app.ts
+++ site/app.ts
@@ -5,53 +5,68 @@ import "leaflet.markercluster";
 
 declare const L: any;
 
-import apartments from "./apartment";
+import { Apartment } from "./apartment";
 import { MarkerManager } from "./markers/marker_manager";
-import { Filter } from "./filter";
+// import { Filter } from "./filter";
 import { Persister } from "./persister";
-
-const quebecLocation = {
-  lat: 46.82,
-  lng: -71.3
-};
 
 const minZoom = 9;
 const defaultZoom = minZoom + 2;
+let map: any;
 
-const map = L.map("mapContainer").setView(quebecLocation, defaultZoom);
+function fetchApartments() {
+  const [city, ad_type] = window.location.pathname.split("/").filter(Boolean);
+  const jsonUrl = `/${city}-${ad_type}.json`;
 
-const accessToken =
-  "pk.eyJ1IjoiYWRhbWhhbW1lcyIsImEiOiJjamQxczNrajQyd25kMndvNWR6cGdqYWl2In0.30k-mIhdJr0otiiSv8mQ-w";
-const attribution =
-  'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://createivecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery @ <a href="http://mapbox.com">Mapbox</a>';
-L.tileLayer(
-  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
-  {
-    attribution,
-    minZoom,
-    id: "mapbox.streets",
-    accessToken
-  }
-).addTo(map);
-
-const apartment_cluster = L.markerClusterGroup({
-  disableClusteringAtZoom: 15,
-  spiderfyOnMaxZoom: false
-});
-map.addLayer(apartment_cluster);
-
-const apartment_set = new Set();
-for (let [_, apartment] of apartments) {
-  apartment_set.add(apartment);
+  fetch(jsonUrl).then(response => response.json().then(loadApartments));
 }
 
-const persister = new Persister(apartment_set);
-const marker_manager = new MarkerManager(
-  apartment_cluster,
-  apartment_set,
-  persister
-);
-marker_manager.displayAll();
+function loadApartments(data: any) {
+  const location = {
+    lat: data.city.latitude,
+    lng: data.city.longitude
+  };
+  map.setView(location, defaultZoom);
 
-const filter = new Filter(apartment_set, marker_manager);
-filter.filter();
+  const apartment_cluster = L.markerClusterGroup({
+    disableClusteringAtZoom: 15,
+    spiderfyOnMaxZoom: false
+  });
+
+  map.addLayer(apartment_cluster);
+  const apartments: Apartment[] = data.offers;
+
+  const apartment_set = new Set(apartments);
+
+  const persister = new Persister(apartment_set);
+  const marker_manager = new MarkerManager(
+    apartment_cluster,
+    apartment_set,
+    persister
+  );
+  marker_manager.displayAll();
+
+  // const filter = new Filter(apartment_set, marker_manager);
+  // filter.filter();
+}
+
+function main() {
+  map = L.map("mapContainer");
+  const accessToken =
+    "pk.eyJ1IjoiYWRhbWhhbW1lcyIsImEiOiJjamQxczNrajQyd25kMndvNWR6cGdqYWl2In0.30k-mIhdJr0otiiSv8mQ-w";
+  const attribution =
+    'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://createivecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery @ <a href="http://mapbox.com">Mapbox</a>';
+  L.tileLayer(
+    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+    {
+      attribution,
+      minZoom,
+      id: "mapbox.streets",
+      accessToken
+    }
+  ).addTo(map);
+
+  fetchApartments();
+}
+
+window.addEventListener("load", main);
